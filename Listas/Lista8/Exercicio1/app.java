821e27238cd76c9523744218b53c4d0499f9cf0d
natanSMS <natansms07@gmail.com>
2024-06-14 22:57:31 -0300

    Lista 8 - Tratamento de excecoes

--- Listas/Lista8/Exercicio1/app.java
+++ Listas/Lista8/Exercicio1/app.java
@@ -1,1 +1,21 @@
-a
+import exceptions.PrecoInvalidoException;
+import exceptions.NomeInvalidoException;
+import loja.Produto;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Produto produto1 = new Produto("Chocolate", 3.50);
+        
+        try {
+            Produto produto2 = new Produto("MeM", 3.50);
+        } catch(NomeInvalidoException exception ) {
+            System.out.println(exception.getMessage());
+        }
+
+        try {
+            Produto produto2 = new Produto("Pilha", 0);
+        } catch(PrecoInvalidoException exception ) {
+            System.out.println(exception.getMessage());
+        }
+    }
+}
--- Listas/Lista8/Exercicio1/loja/Produto.java
+++ Listas/Lista8/Exercicio1/loja/Produto.java
@@ -1,23 +1,26 @@
 package loja;
 
-public class Loja {
+import exceptions.NomeInvalidoException;
+import exceptions.PrecoInvalidoException;
+
+public class Produto {
     private String nome;
     private double preco;
 
-    public Loja (String nome, double preco){
-        this.nome = nome;
-        this.preco = preco;
+    public Produto (String nome, double preco) throws Exception{
+        setNome(nome);
+        setPreco(preco);
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) throws Execption {
-        if(nome.size() > 3) {
+    public void setNome(String nome) throws NomeInvalidoException {
+        if(nome.length() > 3) {
             this.nome = nome;
         } else {
-            ...;
+            throw new NomeInvalidoException();
         }
     }
 
@@ -25,9 +28,9 @@ public class Loja {
         return preco;
     }
 
-    public void setPreco(double preco) {
+    public void setPreco(double preco) throws PrecoInvalidoException{
         if(preco <= 0) {
-            ...;
+            throw new PrecoInvalidoException();
         } else {
             this.preco = preco;
         }
